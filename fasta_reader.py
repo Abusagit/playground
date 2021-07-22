dbcec23354b08d8a3b057607b0bd221748458fce
Darji <mirotvorez00@gmail.com>
2021-07-22 17:20:16 +0300

    feat/update: block_reader.py

--- fasta_reader.py
+++ fasta_reader.py
@@ -50,8 +50,8 @@ def iter_fasta_reader(fasta_file_name):
 
 
 class FastaData(block_reader.BlockData):
-    def __init__(self, path, new_block_symbol='>'):
-        super().__init__(path, new_block_symbol)
+    def __init__(self, path):
+        super().__init__(path, new_block_symbol='>')
 
     def iter_block_file(self, new_block_symbol='>'):
         yield from super(FastaData, self).iter_block_file()
